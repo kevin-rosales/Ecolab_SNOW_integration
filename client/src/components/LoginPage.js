4b6ce295afe94b6c3a44c283de9fd194338ef65d
kevin-rosales <kevin.rosales@navomi.com>
2021-04-30 12:47:45 -0400

    added some logic that checks to see if access_token is present in storage

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
+import * as localStorageService from "./LocalStorageService";
 
 class LoginPage extends Component {
+  state = {
+    login: "",
+    tokenMissing: null,
+  };
+
+  componentDidMount() {
+    const tokens = localStorageService.getAccessToken();
+    console.log(tokens);
+    if (tokens === null || tokens === undefined) {
+      this.setState({ login: "Sign In", tokenMissing: true });
+    } else {
+      window.location.href = "/user";
+    }
+  }
+
   handleCLick = (e) => {
     e.preventDefault();
     window.open(
       "https://ecolabqa.service-now.com/oauth_auth.do?response_type=code&redirect_uri=https://quiet-everglades-59480.herokuapp.com/callback&client_id=0d18a00317aba4d424afc79f43a1effa&state=snow"
-      );
-      window.location.href = "/user";
+    );
+
+    const tokens = localStorageService.getAccessToken();
+    console.log(tokens);
+
+    window.location.href = "/user";
   };
-  
+
   render() {
     return (
       <div id="Mspage">
-        <h1>Sign in </h1>
+        <br />
+        {this.state.tokenMissing === null ||
+        this.state.tokenMissing === true ? (
+          <h1>{this.state.login}</h1>
+        ) : null}
         <hr />
         <div>
           <button
